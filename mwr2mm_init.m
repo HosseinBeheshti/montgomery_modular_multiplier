5d1b9a6beffc8b477a24a5f97c0719f6bd75fbba
HosseinBeheshti <h.beheshti92@gmail.com>
2022-07-27 15:33:16 +0430

    add comment

--- mwr2mm_init.m
+++ mwr2mm_init.m
@@ -23,7 +23,8 @@ E = fi(hex2dec("4741"),0,max_n_bit,0);
 N = fi(hex2dec("8167"),0,max_n_bit,0);
 K = fi(mod((4)^max_n_bit,double(N)),0,max_n_bit,0);
 Q = zeros(max_n_bit,1,'like',K);
-% MM_core
+
+% implementation with MM_core
 P = MM_core(M,K,N,max_n_bit);
 Q = MM_core(K,1,N,max_n_bit);
 for i = max_n_bit:-1:1
@@ -37,7 +38,7 @@ enc_msg_mn = MM_core(Q,1,N,max_n_bit);
 disp_value = ['enc_msg_mn message is: ',num2str(dec2hex(enc_msg_mn))];
 disp(disp_value)
 
-% MWR2MM_core
+% implementation with  MWR2MM_core
 P_mwr2mm = MWR2MM_core(M,K,N,max_n_bit,word_length,number_of_pe);
 Q_mwr2mm = MWR2MM_core(K,fixed_point_one,N,max_n_bit,word_length,number_of_pe);
 for i = max_n_bit:-1:1
